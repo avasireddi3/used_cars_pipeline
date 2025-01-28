7adf79cbeb95e4d1ae95be6829ca168e63cf43f6
Aditya Vasireddi <avasireddi3@gmail.com>
2025-01-28 13:00:56 +0530

    finished writing basic workflow to get listing and variant data and store them in a database

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 from src.ingest import api_scraper
 from src.ingest.api_scraper import generate_brands_dict, get_brand_cars
+from src.persist import load
+from src.persist.load import check_variant_exists, insert_variant, check_listing_exists
 
 
 def main():
@@ -8,10 +10,26 @@ def main():
     brands_dict = generate_brands_dict(listings_raw)
 
     #TODO: see if processing can be asyncronized
-    for brand in brands_dict:
-        brand_cars = get_brand_cars(brand, brands_dict)
-        for car in brand_cars[:1]:
-            print(json.dumps(car, indent=4))
+
+    # for brand in brands_dict:
+    #     brand_cars = get_brand_cars(brand, brands_dict)
+
+    brand_cars = get_brand_cars("bmw",brands_dict)
+
+    try:
+        load.initialize_tables()
+    except ValueError:
+        print("table initialization failed")
+
+    for car in brand_cars:
+        listing_id = car['id']
+        if not check_listing_exists(listing_id):
+            load.insert_listing(list(car.values()))
+        variant_id = car['variant_id']
+        if not check_variant_exists(variant_id):
+            variant_info = api_scraper.get_variant_info(variant_id)
+            insert_variant(variant_id,variant_info)
+
 
 
 
