4a21dc8a2331be1073a99461a3e50de6dee32cec
Aditya Vasireddi <avasireddi3@gmail.com>
2025-01-28 12:58:15 +0530

    minor changes to insert functions, added check_listing_exists function fixed check_variant_exists function

--- src/persist/load.py
+++ src/persist/load.py
@@ -6,62 +6,73 @@ conn = psycopg2.connect(database= "usedcars2",
                          host = credentials_test.host,
                          password = credentials_test.password,
                          port = 5432)
-cur = conn.cursor()
+
 
 def initialize_tables()->None:
 #create table for used car listings
-    cur.execute("""CREATE TABLE IF NOT EXISTS demo1(
-                    id INTEGER PRIMARY KEY,
-                    variantid INTEGER,
-                    bodytype VARCHAR,
-                    city VARCHAR,
-                    price INTEGER,
-                    fueltype VARCHAR,
-                    mileage VARCHAR,
-                    make VARCHAR,
-                    model VARCHAR,
-                    gear VARCHAR,
-                    owner VARCHAR)
-                """)
+    with conn.cursor() as cur:
+        cur.execute("""CREATE TABLE IF NOT EXISTS listings_demo(
+                        id INTEGER PRIMARY KEY,
+                        variant_id INTEGER,
+                        body_type VARCHAR,
+                        city VARCHAR,
+                        price INTEGER,
+                        fuel_type VARCHAR,
+                        mileage VARCHAR,
+                        make VARCHAR,
+                        model VARCHAR,
+                        gear VARCHAR,
+                        owner VARCHAR)
+                    """)
 
 #create table for car variant information
-    cur.execute("""CREATE TABLE IF NOT EXISTS variant_details(
-                    variant_id INTEGER PRIMARY KEY,
-                    engine_cc INTEGER,
-                    ground_clearance_mm INTEGER,
-                    mileage_kmpl REAL,
-                    drive_type VARCHAR,
-                    seating INTEGER,
-                    power REAL,
-                    cylinders INTEGER,
-                    gearbox VARCHAR,
-                    top_speed_kmph REAL,
-                    enc VARCHAR,
-                    length_mm INTEGER,
-                    width_mm INTEGER,
-                    height_mm INTEGER
-                    )
-                """)
+        cur.execute("""CREATE TABLE IF NOT EXISTS variants_demo(
+                        variant_id INTEGER PRIMARY KEY,
+                        engine_cc VARCHAR,
+                        ground_clearance_mm VARCHAR,
+                        mileage_kmpl VARCHAR,
+                        drive_type VARCHAR,
+                        seating VARCHAR,
+                        power VARCHAR,
+                        cylinders VARCHAR,
+                        gearbox VARCHAR,
+                        top_speed_kmph VARCHAR,
+                        enc VARCHAR,
+                        length_mm VARCHAR,
+                        width_mm VARCHAR,
+                        height_mm VARCHAR)
+                    """)
 
 def check_variant_exists(variant_id:int)->bool:
-    #check if variant info already exists in table
-    check_variant = cur.execute("""SELECT * 
-                                                FROM variant_details
-                                                WHERE variant_id = VALUES(%s)""", variant_id)
-    if check_variant:
-        return True
-    else:
-        return False
+    #check if variant info already exists in
+    with conn.cursor() as cur:
+        cur.execute("""SELECT * 
+                            FROM variants_demo
+                            WHERE variant_id = %s""", [variant_id])
+        if not cur.fetchall():
+            return False
+        else:
+            return True
 
-def insert_variant(variant_id:int,variant_info:dict)->None:
+def check_listing_exists(listing_id:int)->bool:
+    #check if listing info already exists in table
+    with conn.cursor() as cur:
+        cur.execute("""SELECT * 
+                            FROM listings_demo
+                            WHERE id = %s""", [listing_id])
+        if not cur.fetchall():
+            return False
+        else:
+            return True
+
+def insert_variant(variant_id:int,variant_info:list)->None:
     #insert variant info into database
-    cur.execute("""INSERT INTO variant_details VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
-                (variant_id,
-                 variant_info['data']['carSpecification']["top"]["engine"]
-                 )
-                )
+    with conn.cursor() as cur:
+        cur.execute("""INSERT INTO variants_demo VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
+                    variant_info)
 
 def insert_listing(listing_info:list)->None:
     #insert listing into database
-    cur.execute("""INSERT INTO demo1 VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", listing_info)
-    conn.commit()
+    with conn.cursor() as cur:
+        cur.execute("""INSERT INTO listings_demo VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", listing_info)
+        conn.commit()
