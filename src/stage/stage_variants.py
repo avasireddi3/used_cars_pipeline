44f15ca6e902deaaf12f81cbef4c0de6d24f99b2
Aditya Vasireddi <avasireddi3@gmail.com>
2025-02-01 12:29:50 +0530

    fixed a bug where duplicate variants where being stored in the variants database

--- src/stage/stage_variants.py
+++ src/stage/stage_variants.py
@@ -3,12 +3,16 @@ import polars as pl
 
 def find_missing_variants(listings_df: pl.DataFrame, variants_df: pl.DataFrame) -> list:
     """get missing variant ids from a listings dataframe"""
-    missing_variants = listings_df.join(
+    missing_variants = (listings_df.join(
         other=variants_df,
         how="left",
         left_on="variant_id",
         right_on="variant_id",
         suffix="_1",
         coalesce=False
-    ).filter(pl.col("variant_id_1").is_null())
-    return missing_variants.get_column("variant_id").to_list()
+    ).filter(pl.col("variant_id_1").is_null()).
+                        get_column("variant_id").
+                        unique().
+                        to_list())
+    print(missing_variants)
+    return missing_variants
