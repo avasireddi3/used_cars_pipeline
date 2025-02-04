05e39b897ccb2c5b5ef42ac8e18e228e0b6acefc
Aditya Vasireddi <avasireddi3@gmail.com>
2025-02-04 18:45:00 +0530

    store request related constants such as header, api urls in a separate file

---
+++ src/extract/extract_constants.py
@@ -0,0 +1,7 @@
+user_headers = {
+        "cookie": """cd_session_id=b4d275df-1a63-4456-8fd4-88da87f0795d;firstUTMParamter=direct%23none%23null""",
+        "User-Agent": """Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36""",
+    }
+
+listings_url = "https://listing.cardekho.com/api/v1/srp-listings"
+variants_url = "https://www.cardekho.com/api/v1/usedcar/specs"
