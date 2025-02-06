4735d530b64389801cf6dc7d3110cdb5e6c8b6b1
Aditya Vasireddi <avasireddi3@gmail.com>
2025-02-06 15:41:22 +0530

    added components to make a minimum viable pipeline and restructured dependencies to improve efficiency

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -1,10 +1,13 @@
 from airflow.decorators import dag
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.models.baseoperator import chain, chain_linear
 from pendulum import datetime
 from lib.load.load_listings import read_data
 from lib.extract.extract_brand_mapping import brand_mapping
 from lib.extract.extract_listings_data import listings_data
-from lib.transform.transform_listings import full_transform
+from lib.extract.extract_variants import variants_data
+from lib.transform.transform_listings import full_transform_listings
+from lib.transform.transform_variants import full_transform_variants
 from lib.load.load_listings import insert_data
 
 @dag(
@@ -26,15 +29,48 @@ def test_workflow():
         conn_id = "postgres_default",
         sql = "sql/new_listings.sql"
     )
+
+    find_new_variants = SQLExecuteQueryOperator(
+        task_id = "find_new_variants",
+        conn_id = "postgres_default",
+        sql = "sql/find_new_variants.sql"
+    )
+
+    insert_new_listings = SQLExecuteQueryOperator(
+        task_id = "insert_new_listings",
+        conn_id = "postgres_default",
+        sql = "sql/insert_new_listings.sql"
+    )
+
+    update_sold_listings = SQLExecuteQueryOperator(
+        task_id = "update_sold_listings",
+        conn_id = "postgres_default",
+        sql = "sql/update_sold_listings.sql"
+    )
+
+    update_unsold_listings = SQLExecuteQueryOperator(
+        task_id = "update_unsold_listings",
+        conn_id = "postgres_default",
+        sql = "sql/update_unsold_listings.sql"
+    )
+
     brands = brand_mapping()
     data = listings_data.expand(brand_mapping=brands)
-    (full_transform.expand(passed=data)
-        >>insert_data()
-        >>read_data()
-        >>init_tables
-        >>find_new_listings)
-
-
+    transformed_data = full_transform_listings.expand(passed=data)
+    variants = variants_data()
+    # chain_linear(transformed_data
+    #  ,insert_data()
+    #  ,init_tables
+    #  ,[find_new_listings,find_new_variants]
+    #  ,[update_sold_listings,
+    #    update_unsold_listings,
+    #    insert_new_listings,
+    #    variants_data()]
+    #  ,full_transform_variants())
+    transformed_data>>insert_data()>>init_tables>>[find_new_listings,find_new_variants]
+    find_new_listings>>[update_sold_listings,update_unsold_listings,insert_new_listings]
+    find_new_variants>>variants
+    full_transform_variants(variants)
 
 
 test_workflow()
