9fd0673ccf4b155b9cfe8262e524b099b468b830
Aditya Vasireddi <avasireddi3@gmail.com>
2025-02-06 15:34:33 +0530

    store intermediary csv files for listings in a dedicated subdirectory on the mounted storage

--- dags/lib/transform/transform_listings.py
+++ dags/lib/transform/transform_listings.py
@@ -1,7 +1,5 @@
 import polars as pl
-from pathlib import Path
 from airflow.decorators import task
-from airflow.operators.python import get_current_context
 
 
 
@@ -27,12 +25,12 @@ def initialize_df(data: list) -> pl.DataFrame:
 
 
 @task
-def full_transform(passed:tuple[str,list[tuple]])->tuple[int,int]:
+def full_transform_listings(passed:tuple[str,list[tuple]])->tuple[int,int]:
     brand=passed[0]
     data=passed[1]
     df = initialize_df(data)
     df_final = df.with_columns(pl.col("mileage").str.replace_all(",", "").cast(pl.Int32))
-    df_final.write_csv(f"/sources/tmp/{brand}.csv",)
+    df_final.write_csv(f"/sources/tmp/listings/{brand}.csv",)
     return df_final.shape
 
 def main():
