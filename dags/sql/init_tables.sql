b46005b2f6ec88f1fe6cd4d4e449d40c165454d8
Aditya Vasireddi <avasireddi3@gmail.com>
2025-02-06 10:49:03 +0530

    initialize tables for variants and sold status

--- dags/sql/init_tables.sql
+++ dags/sql/init_tables.sql
@@ -11,3 +11,26 @@ CREATE TABLE IF NOT EXISTS test_listings
     gear varchar(30),
     owner varchar(30)
     );
+
+CREATE TABLE IF NOT EXISTS test_variants
+    (variant_id int PRIMARY KEY,
+    engine_cc int,
+    ground_clearance_mm int,
+    mileage_kmpl int,
+    drive_type varchar(30),
+    seating_capacity int,
+    power int,
+    cylinders int,
+    gearbox varchar(30),
+    top_speed_kmph int,
+    enc varchar(30),
+    length_mm int,
+    width_mm int,
+    height_mm int
+    );
+
+CREATE TABLE IF NOT EXISTS test_status
+    (id int,
+    status varchar(10),
+    update_time timestamp
+    );
