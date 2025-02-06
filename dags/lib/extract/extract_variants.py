a79194b3fd0079e1950827c66f57c84fac8c5da7
Aditya Vasireddi <avasireddi3@gmail.com>
2025-02-06 15:31:59 +0530

    write new variants to an intermediary storage in the form of a csv in the variants_data() function

--- dags/lib/extract/extract_variants.py
+++ dags/lib/extract/extract_variants.py
@@ -2,17 +2,19 @@ import requests
 import json
 from .extract_constants import variants_url,user_headers
 from airflow.decorators import task
-
+import polars as pl
+from sqlalchemy import create_engine
 
 @task
-def variants_data(variant_ids:list):
+def variants_data(**context):
     """get variants info for a given list of variant ids"""
     payload = ""
     url = variants_url
     headers = user_headers
     variants_info = []
+    variant_ids = context['ti'].xcom_pull(task_ids='find_new_variants',key='return_value')
     for variant_id in variant_ids:
-
+        variant_id = variant_id[0]
         querystring_variant = {
             "cityId": "105",
             "connectoid": "a223ce8e-09eb-2670-52c8-170d94d8ddad",
@@ -75,5 +77,24 @@ def variants_data(variant_ids:list):
                 variant_info[name] = 0
 
         variants_info.append(list(variant_info.values()))
+    variants_df = pl.DataFrame(data=variants_info,
+                               schema={
+                                   "variant_id": pl.Int32,
+                                   "engine_cc": pl.String,
+                                   "ground_clearance_mm": pl.String,
+                                   "mileage_kmpl": pl.String,
+                                   "drive_type": pl.String,
+                                   "seating_capacity": pl.String,
+                                   "power": pl.String,
+                                   "cylinders": pl.String,
+                                   "gearbox": pl.String,
+                                   "top_speed_kmph": pl.String,
+                                   "enc": pl.String,
+                                   "length_mm": pl.String,
+                                   "width_mm": pl.String,
+                                   "height_mm": pl.String,
+                               },
+                                orient='row',)
+    variants_df.write_csv("/sources/tmp/variants/new_variants.csv")
 
-    return variants_info
+    return f"loaded {len(variants_info)} new variants"
