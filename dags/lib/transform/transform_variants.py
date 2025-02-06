dea71463405c23ee699c3f6dbcb37d8e93a3eec5
Aditya Vasireddi <avasireddi3@gmail.com>
2025-02-06 15:35:15 +0530

    store intermediary csv files for new variants in a dedicated subdirectory on the mounted storage

--- dags/lib/transform/transform_variants.py
+++ dags/lib/transform/transform_variants.py
@@ -1,5 +1,6 @@
 import polars as pl
-
+from sqlalchemy import create_engine
+from airflow.decorators import task
 
 def initialize_df(data: list) -> pl.DataFrame:
     df = pl.DataFrame(
@@ -29,7 +30,7 @@ def string_operations(df: pl.DataFrame) -> pl.DataFrame:
         pl.col("engine_cc").str.replace_all(r"\D", "").cast(pl.Int16),
         pl.col("ground_clearance_mm").str.replace_all(" mm", "").cast(pl.Int16),
         pl.col("seating_capacity").cast(pl.Int8),
-        pl.col("mileage_kmpl").str.replace_all(" kmpl", "").cast(pl.Int16),
+        pl.col("mileage_kmpl").str.replace_all(" kmpl", "").cast(pl.Float32),
         pl.col("power").str.replace_all(" bhp", "").cast(pl.Float32),
         pl.col("top_speed_kmph").str.replace_all(r"\D", "").cast(pl.Int16),
         pl.col("length_mm").str.replace_all(" mm", "").cast(pl.Int16),
@@ -37,7 +38,16 @@ def string_operations(df: pl.DataFrame) -> pl.DataFrame:
         pl.col("height_mm").str.replace_all(" mm", "").cast(pl.Int16),
     )
 
-def full_transform(data:list)->pl.DataFrame:
-    df = initialize_df(data)
+@task
+def full_transform_variants(*args,**kwargs)->tuple[int,int]:
+    db_url = "postgresql+psycopg2://airflow:airflow@postgres/usedcars2"
+    engine = create_engine(db_url)
+    df = pl.read_csv("/sources/tmp/variants/new_variants.csv")
     df_final = string_operations(df)
-    return df_final
+    with engine.connect() as conn:
+        df_final.write_database(
+            table_name="variants_demo",
+            connection=conn,
+            if_table_exists="append"
+        )
+    return df_final.shape
